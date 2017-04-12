035fbbb57967f16d729b04fa6424c2f40d386b96
ajlandau <amyjlandau@gmail.com>
2017-04-12 12:00:54 -0400

    defines

--- H_bridge.cpp
+++ H_bridge.cpp
@@ -41,11 +41,11 @@ H_Bridge::H_Bridge(int nodei, int nodej, int nodek, int nodel, int state){
     Diode(nodel, nodei);
     
     switch(state){
-        case 0:
+        case 0: // "off" state
             
-        case 1:
+        case 1: // Forward state
             
-        case 2:
+        case 2: //Reverse state
             
             
     }
